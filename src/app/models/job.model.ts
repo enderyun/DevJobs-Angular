09c3aa90f15b322a7bb71a829d04226b103f7740
enderyun <enderyum1524@gmail.com>
2026-03-14 17:03:59 -0500

    creacion de componentes, homepage e imports

---
+++ src/app/models/job.model.ts
@@ -0,0 +1,18 @@
+export interface Job {
+  id: string;
+  titulo: string;
+  empresa: string;
+  ubicacion: string;
+  descripcion: string;
+  data: {
+    technology: string | string[];
+    modalidad: string;
+    nivel: string;
+  };
+  content: {
+    descripcion: string;
+    responsibilities: string;
+    requirements: string;
+    about: string;
+  };
+}
